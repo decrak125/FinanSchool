81a50b3f3b49e474f5e42381d1b11a2024c2eac2
9huit <nantenkf@gmail.com>
2025-09-12 12:08:45 +0300

    font

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -7,4 +7,16 @@ import { RouterLink, RouterView } from "vue-router";
 </template>
 
 <style scoped>
+      @font-face {
+      font-family: "Stara";
+      src: url("../public/fonts/Stara-Medium.woff2") format("woff2"),
+            url("../public/fonts/Stara-Medium.woff") format("woff");
+      font-weight: normal;
+      font-style: normal;
+      font-display: swap;
+      }
+
+      body {
+      font-family: "Stara", sans-serif;
+      }
 </style>
